aff6cdd7d8687b2f5a0e00d0b5ba5a959c2761c5
drehno <145988314+drehno@users.noreply.github.com>
2023-09-29 20:58:49 +0200

    Update TTTPlayingFrame.java

--- TicTacToeWithGUI/TTTPlayingFrame.java
+++ TicTacToeWithGUI/TTTPlayingFrame.java
@@ -1,13 +1,7 @@
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
 
 public class TTTPlayingFrame extends JFrame implements ActionListener {
 
@@ -17,20 +11,32 @@ public class TTTPlayingFrame extends JFrame implements ActionListener {
     String spieler1xOdero = " ";
     String spieler2xOdero = " ";
     JTextField auswahl = new JTextField();
+    JLabel ueberschrift = new JLabel();
+    JPanel titelPanel = new JPanel();
 
     TTTPlayingFrame(String oOderx) {
         spieler1xOdero = oOderx;
-        if (spieler1xOdero.compareTo("X") == 1) {
+        if (spieler1xOdero.compareTo("X") == 0) {
             spieler2xOdero = "O";
         } else {
             spieler2xOdero = "X";
         }
+
+        ueberschrift.setBackground(new Color(95, 158, 160));
+        ueberschrift.setForeground(new Color(0, 0, 0));
+        ueberschrift.setFont(new Font("Comic Sans", Font.BOLD, 50));
+        ueberschrift.setHorizontalAlignment(JLabel.CENTER);
+        ueberschrift.setText("TicTacToe");
+        ueberschrift.setOpaque(true);
+
+        titelPanel.setLayout(new BorderLayout());
 
         this.setTitle("TicTacToe");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500, 500);
         this.setVisible(true);
         this.getContentPane().setBackground(new Color(95, 158, 160));
+        this.setLayout(new BorderLayout());
 
         spielfeld.setLayout(new GridLayout(3, 3));
         spielfeld.setBackground(new Color(95, 158, 160));
@@ -42,7 +48,9 @@ public class TTTPlayingFrame extends JFrame implements ActionListener {
             buttonfeld[i].addActionListener(this);
         }
 
+        titelPanel.add(ueberschrift);
         this.add(spielfeld);
+        this.add(titelPanel, BorderLayout.NORTH);
     }
 
     @Override
@@ -70,36 +78,43 @@ public class TTTPlayingFrame extends JFrame implements ActionListener {
     }
 
     private void check(String xOdero) {
-        if (buttonfeld[0].getText().compareTo(xOdero) == 1 && buttonfeld[1].getText().compareTo(xOdero) == 1
-                && buttonfeld[3].getText().compareTo(xOdero) == 1) {
+        if (buttonfeld[0].getText().compareTo(xOdero) == 0 && buttonfeld[1].getText().compareTo(xOdero) == 0
+                && buttonfeld[3].getText().compareTo(xOdero) == 0) {
             checkWinner(0, 1, 2, xOdero);
-        } else if (buttonfeld[3].getText().compareTo(xOdero) == 1 && buttonfeld[4].getText().compareTo(xOdero) == 1
-                && buttonfeld[5].getText().compareTo(xOdero) == 1) {
+        } else if (buttonfeld[3].getText().compareTo(xOdero) == 0 && buttonfeld[4].getText().compareTo(xOdero) == 0
+                && buttonfeld[5].getText().compareTo(xOdero) == 0) {
             checkWinner(3, 4, 5, xOdero);
-        } else if (buttonfeld[6].getText().compareTo(xOdero) == 1 && buttonfeld[7].getText().compareTo(xOdero) == 1
-                && buttonfeld[8].getText().compareTo(xOdero) == 1) {
+        } else if (buttonfeld[6].getText().compareTo(xOdero) == 0 && buttonfeld[7].getText().compareTo(xOdero) == 0
+                && buttonfeld[8].getText().compareTo(xOdero) == 0) {
             checkWinner(6, 7, 8, xOdero);
-        } else if (buttonfeld[0].getText().compareTo(xOdero) == 1 && buttonfeld[3].getText().compareTo(xOdero) == 1
-                && buttonfeld[6].getText().compareTo(xOdero) == 1) {
+        } else if (buttonfeld[0].getText().compareTo(xOdero) == 0 && buttonfeld[3].getText().compareTo(xOdero) == 0
+                && buttonfeld[6].getText().compareTo(xOdero) == 0) {
             checkWinner(0, 3, 6, xOdero);
-        } else if (buttonfeld[1].getText().compareTo(xOdero) == 1 && buttonfeld[4].getText().compareTo(xOdero) == 1
-                && buttonfeld[7].getText().compareTo(xOdero) == 1) {
+        } else if (buttonfeld[1].getText().compareTo(xOdero) == 0 && buttonfeld[4].getText().compareTo(xOdero) == 0
+                && buttonfeld[7].getText().compareTo(xOdero) == 0) {
             checkWinner(1, 4, 7, xOdero);
-        } else if (buttonfeld[2].getText().compareTo(xOdero) == 1 && buttonfeld[5].getText().compareTo(xOdero) == 1
-                && buttonfeld[8].getText().compareTo(xOdero) == 1) {
+        } else if (buttonfeld[2].getText().compareTo(xOdero) == 0 && buttonfeld[5].getText().compareTo(xOdero) == 0
+                && buttonfeld[8].getText().compareTo(xOdero) == 0) {
             checkWinner(3, 4, 5, xOdero);
-        } else if (buttonfeld[0].getText().compareTo(xOdero) == 1 && buttonfeld[4].getText().compareTo(xOdero) == 1
-                && buttonfeld[8].getText().compareTo(xOdero) == 1) {
+        } else if (buttonfeld[0].getText().compareTo(xOdero) == 0 && buttonfeld[4].getText().compareTo(xOdero) == 0
+                && buttonfeld[8].getText().compareTo(xOdero) == 0) {
             checkWinner(0, 4, 8, xOdero);
-        } else if (buttonfeld[2].getText().compareTo(xOdero) == 1 && buttonfeld[4].getText().compareTo(xOdero) == 1
-                && buttonfeld[6].getText().compareTo(xOdero) == 1) {
+        } else if (buttonfeld[2].getText().compareTo(xOdero) == 0 && buttonfeld[4].getText().compareTo(xOdero) == 0
+                && buttonfeld[6].getText().compareTo(xOdero) == 0) {
             checkWinner(2, 4, 6, xOdero);
         }
 
     }
 
     private void checkWinner(int a, int b, int c, String winner) {
+        buttonfeld[a].setBackground(Color.GREEN);
+        buttonfeld[b].setBackground(Color.GREEN);
+        buttonfeld[c].setBackground(Color.GREEN);
 
+        for (int i = 0; i < 9; i++) {
+            buttonfeld[i].setEnabled(false);
+        }
+        ueberschrift.setText(winner + " wins");
     }
 
 }
